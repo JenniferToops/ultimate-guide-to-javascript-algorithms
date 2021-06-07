8fb56f73d8d9ee690a0927d48dad422c8b99da0f
Jennifer Toops <jennifertoops@gmail.com>
2021-06-07 13:18:12 -0400

    vowelsCounter

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,51 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
-
 function reverseString(text) {
-    // Code goes here
+    return text.split("").reverse().join("");
 }
 
+// loop option, decremeting
+
+function reverseString(text) {
+    let result = ""
+    for (let i = text.length - 1; i >= 0; i--) {
+        result += text[i]
+    }
+    return result
+}
+
+// kind of loop "for-of" 
+
+function reverseString(text) {
+    let result = ""
+
+    for (let char of text) {
+        result = char + result
+    }
+    return result
+}
+
+// recursive way
+function reverseString(text) {
+    if (text === "") {
+        return ""
+    } else {
+        return reverseString(text.substring(1)) + text [0]
+    }
+}
+
+// reduce
+
+function reverseString(text) {
+    return text.split("").reduce((acc, char) => char + acc, '')
+}
+
+// reduce with spread
+
+function reverseString(text) {
+    return [... text].reduce((acc, char) => char + acc, '')
+}
 
 
 module.exports = reverseString
--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -2,11 +2,35 @@
 Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
+const vowels = ["a", "e", "i", "o", "u"]
+function vowelsCounter(text) {
 
+    let counter = 0;
+
+    for (let letter of text.toLowerCase()) {
+        if (vowels.includes(letter)) {
+        counter++
+        }
+    }
+    return counter
+}
+
+// use regular expression to return the number of of vowels
+// going to use match() with regex to figure out # of vowels (use /gi)
 
 function vowelsCounter(text) {
-    // Code goes here
+    const regex = /[aeiou]/gi
+    let howMany = text.match(regex)
+    return howMany.length
 }
+
+
+
+
+
+
+
+
 
 
 
