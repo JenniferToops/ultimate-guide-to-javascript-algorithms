73a15eb105906c01bce833c12516c23ad8eacc5b
Jennifer Toops <jennifertoops@gmail.com>
2021-06-08 13:12:25 -0400

    longestWord

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -3,11 +3,23 @@ Given two strings of equal length, calculate and return the the hamming distance
 E.g hammingDistance('rover', 'river') // should return 1
 */
 
-
-
-
+// the test throws an error for this code... no idea why since I copied and pasted it
 function hammingDistance(stringA, stringB) {
-    // Code goes here
+    let result = 0
+
+    if (stringA.length == stringB.length) {
+
+        for (let i = 0; i < stringA.length; i++) {
+            if (stringA++[++i].toLowerCase() != stringB++[++i].toLowerCase()) {
+                result++
+            }
+        }
+
+        return result
+
+    } else {
+        throw new Error('Strings do not have equal length')
+    }
 }
 
 
--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -4,9 +4,26 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
-
+// for-loop
 function longestWord(text) {
-    // Code goes here
+    //turn the string into an array:
+    let wordArray = text.split(' ')
+    //declare the initial maxLength value
+    let maxLength = 0
+    //declare the result string
+    let result = ''
+
+    //set up loop to iterate over all of the elements in the array
+    for (let i = 0; i < wordArray.length; i++) {
+        //if the length of the element is greater than the maxLength
+        if (wordArray[i].length > maxLength) {
+            //reassign that element's length to the maxLength variable
+            maxLength = wordArray[i].length
+            //assign that (lengthiest) element to the result variable
+            result = wordArray[i]
+        }
+    } //return the result (element containing the longest word)
+        return result
 }
 
 
